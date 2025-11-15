a09f85034711b747725152e17f87cb55b2e4deea
schweb04 <manelequintero@gmail.com>
2025-11-15 11:26:47 -0400

    include folder

---
+++ include/Preferences.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Settings.hpp"
+
+enum PreferenceType { NO_PREFERENCE, DAYS, HOURS, DAYS_HOURS };
+
+class Preferences
+{
+public:
+    // Preferences(const std::string& description, PreferenceType type,
+    //             const std::vector<Days>& days,
+    //             const std::vector<uint>& hours)
+    //     : description(description), type(type), days(days), hours(hours)
+    //       {}
+
+    const std::string& get_description() const 
+    { 
+        return description; 
+    }
+    const PreferenceType& get_type() const 
+    { 
+        return type; 
+    }
+    const std::vector<Days>& get_days() const 
+    { 
+        return days; 
+    }
+    const std::vector<uint>& get_hours() const 
+    { 
+        return hours; 
+    }
+
+private:
+    std::string description;
+    PreferenceType type;
+    std::vector<Days> days;
+    std::vector<uint> hours;
+};
