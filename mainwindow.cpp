1ead12e3eacc6235f91bf8c29ebe6427889ef43b
Eldry <eldryjesu@gmail.com>
2025-11-16 22:37:13 -0400

    Avances Interfaz

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,29 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Asegurarnos de empezar siempre en la página 0 (el menú principal)
+    ui->stackedWidget->setCurrentIndex(0);
+
+    // CONEXIÓN MANUAL
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::avanzar_ventana);
+
+    connect(ui->backButton, &QPushButton::clicked, this, &MainWindow::volver_ventana);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::avanzar_ventana()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+void MainWindow::volver_ventana()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
